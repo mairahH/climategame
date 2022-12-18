0d07ba426d045af449efbf2572a68687f1f8bbf5
AramBadawi <110118510+AramBadawi@users.noreply.github.com>
2022-12-17 20:43:28 -0500

    jumping working
    jump now works with "space"

--- Archive/Assets/PlayerMovement.cs
+++ Archive/Assets/PlayerMovement.cs
@@ -1,29 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     private float horizontal;
     private float speed = 1f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 3f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-
+    /*
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        /*horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && isGrounded()){
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
@@ -34,7 +35,17 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
-        Flip();
+        Flip();*/
+
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+
     }
 
 
@@ -50,12 +61,30 @@ public class PlayerMovement : MonoBehaviour
     
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
+        //if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        //{
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+        //}
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (context.canceled && rb.velocity.y >0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
     }
 }
